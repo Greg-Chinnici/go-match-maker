f50df1ab64b710b598529ff2c9aa35481670776d
Greg-Chinnici <chinnici@chapman.edu>
2026-03-01 18:43:04 -0800

    tst 200 new players if no csv found

--- bulkQueue.py
+++ bulkQueue.py
@@ -1,17 +1,18 @@
 import csv
 import json
-import sys
 import urllib.request
 import time,random
+from pathlib import Path
 
 QUEUE_API_URL = "http://localhost:8080/queue"
 ACTIVEMATCHES_API_URL = "http://localhost:8080/active-matches"
 REPORT_API_URL = "http://localhost:8080/report"
 CSV_FILE = "glickplayers100diff.csv"
+
 players_sent =0
 
 
-def post_player(player_id, rating, rd, volatility):
+def post_player(player_id=""):
     data = {
         "uid": player_id,
     }
@@ -29,50 +30,62 @@ def post_player(player_id, rating, rd, volatility):
 def submit_result(winner , match_id):
     d = { "match_id" : match_id , "winner_id" : winner}
     req = urllib.request.Request(
-        QUEUE_API_URL,
+        REPORT_API_URL,
         data=json.dumps(d).encode("utf-8"),
         headers={"Content-Type": "application/json"},
         method="POST",
     )
     with urllib.request.urlopen(req) as response:
-        return response.read()
-    
-def main():
+        return json.loads(response.read())
+def csvSource():
     with open(CSV_FILE, newline="") as file:
         reader = csv.reader(file)
         global players_sent
         
         for row in reader:
-            if len(row) < 4:
+            if len(row) < 1:
                 continue
 
-            if players_sent > 40:
-                break
             
             player_id = row[0].strip('"')
-            rating = row[1].strip('"')
-            rd = row[2].strip('"')
-            volatility = row[3].strip('"')
 
             try:
-                post_player(player_id, rating, rd, volatility)
+                post_player(player_id)
                 print(f"Inserted {player_id}")
                 players_sent += 1
             except Exception as e:
                 print(f"Error inserting {player_id}: {e}")
-                
-            sleepTime = random.randint(5,50)
-            time.sleep(sleepTime / 50)
+            time.sleep(0.05)
 
+def autoSource(cnt= 200):
+    global players_sent
+    
+    for i in range(cnt):
+        try:
+            post_player(None)
+            players_sent += 1
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+        time.sleep(0.05)
+    
+
+def main():
+    if (Path(CSV_FILE).exists()): csvSource()
+    else: autoSource()
+                
+    time.sleep(2)
     # after all players are sent
     req = urllib.request.Request(ACTIVEMATCHES_API_URL)
+    time.sleep(1)
+    
     with urllib.request.urlopen(req) as response:
         data = response.read()
         active_matches = json.loads(data)
+        print(f"api found {len(active_matches)} matches")
         for match_id, match in active_matches.items():
             
-            player1 = match["Player1"]
-            player2 = match["Player2"]
+            player1 = match["Player1"]["ID"]
+            player2 = match["Player2"]["ID"]
 
             winner = random.choice([player1, player2])
             r = submit_result(winner , match_id)
@@ -85,7 +98,9 @@ def main():
 
 if __name__ == "__main__":
     players_sent = 0
+    print("Needs a csv file of player-ids")
     try:
         main()
-    except:
+    except Exception as e:
+        print(e)
         print(f"total players queued: {players_sent}")
